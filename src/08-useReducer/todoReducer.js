db1fd13c42d518a52b32f49525aa758953f2e0ea
Johann Sebastian Castillo Vasquez <johannsebastian.castillovasquez@emeal.nttdata.com>
2023-02-09 16:20:57 -0500

    useReducer first class

---
+++ src/08-useReducer/todoReducer.js
@@ -0,0 +1,8 @@
+export const todoReducer = (initialState, action) => {
+    switch (action.type) {
+        case 'ABC':
+            return initialState;
+        default:
+            return initialState;
+    }
+}
