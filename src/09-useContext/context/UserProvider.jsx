233aef2e026d028499d1afb102f8433c484e2135
Sebastian <sebastiancastillo1402@gmail.com>
2023-02-15 15:01:54 -0500

    test init

---
+++ src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { UserContext } from "./UserContext";
+
+// const user = {
+//   id: 123,
+//   name: 'Sebastian Castillo',
+//   email: 'sebas@gmail.com'
+// }
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  return (
+    <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { TodoApp } from "./08-useReducer/TodoApp";
-// import { Padre } from "./07-tarea-memo/Padre";
-// import { CallbackHook } from "./06-memos/CallbackHook";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { AboutPage } from "./09-useContext/AboutPage";
+import { HomePage } from "./09-useContext/HomePage";
+import { LoginPage } from "./09-useContext/LoginPage";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
@@ -10,15 +15,44 @@ import { TodoApp } from "./08-useReducer/TodoApp";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
+// import { CallbackHook } from "./06-memos/CallbackHook";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { Memorize } from "./06-memos/Memorize";
+// import { Padre } from "./07-tarea-memo/Padre";
+// import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
 // import { HooksApp } from "./HooksApp";
 import "./index.css";
 
 // import "./08-useReducer/intro.reducer";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainApp />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+    ],
+  },
+  {
+    path: "/*",
+    element: <Navigate to={"/"} />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-  <TodoApp />
+  <RouterProvider router={router} />
   //</React.StrictMode>,
 );
